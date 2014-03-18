e70c976416505b11ac60ab57520499a6854d7bc8
Felix Krause <flyx@isobeef.org>
2014-03-18 20:24:17 +0100

    Added GtkOSXApplication support
     * added gtkosxapplication.nim, which wraps GtkOSXApplication
     * added an example that shows how to use it
     * added g_object_new to glib2 because it was missing

---
+++ examples/macosx_quartz_ex1.nim
@@ -0,0 +1,63 @@
+import 
+  glib2, gtk2, gtkmacintegration, os
+
+proc destroy(widget: pWidget, data: pgpointer) {.cdecl.} =
+  main_quit()
+
+var
+  application: POSXApplication
+  window: pWidget
+nimrod_init()
+
+# OSX application initialization
+application = POSXApplication(g_object_new(get_type(), nil))
+var menubar = menu_bar_new()
+var filemenu = menu_new()
+var about = menu_item_new("About")
+var separator = separator_menu_item_new()
+var prefs = menu_item_new("Preferences")
+var file = menu_item_new("File")
+var filenew = menu_item_new("New")
+file.set_submenu(filemenu)
+filemenu.append(filenew)
+menubar.append(file)
+application.set_menu_bar(menubar)
+menubar.show_all()
+application.insert_app_menu_item(about, 0)
+application.insert_app_menu_item(separator, 1)
+application.insert_app_menu_item(prefs, 2)
+ready(application)
+
+# Window initialization
+proc do_info_attention_request() {.cdecl.} =
+  os.sleep(1000)
+  discard application.attention_request(INFO_REQUEST)
+
+proc do_critical_attention_request() {.cdecl.} =
+  os.sleep(1000)
+  discard application.attention_request(CRITICAL_REQUEST)
+
+var button_attention_info = button_new("Info")
+var button_attention_critical = button_new("Critical")
+var hbox = hbox_new(true, 10)
+hbox.pack_start(button_attention_info, false, false, 10)
+hbox.pack_end(button_attention_critical, false, false, 10)
+var vbox = vbox_new(true, 10)
+var label = label_new("""
+    Attention requests only work if the application doesn't
+    have focus. To test them, click a button and change the focus elsewhere within
+    one second.""")
+vbox.pack_start(label, false, false, 5)
+vbox.pack_end(hbox, false, false, 10)
+window = window_new(WINDOW_TOPLEVEL)
+PContainer(window).add(vbox)
+discard signal_connect_object(button_attention_info, "clicked", 
+                              SIGNAL_FUNC(macosx_quartz_ex1.do_info_attention_request), 
+                              window)
+discard signal_connect_object(button_attention_critical, "clicked", 
+                              SIGNAL_FUNC(macosx_quartz_ex1.do_critical_attention_request), 
+                              window)
+discard signal_connect(window, "destroy",
+                       SIGNAL_FUNC(macosx_quartz_ex1.destroy), nil)
+window.show_all()
+main()
---
+++ src/gtkmacintegration.nim
@@ -0,0 +1,81 @@
+{.deadCodeElim: on.}
+import 
+  glib2, gdk2, gdk2pixbuf, gtk2
+
+const
+  lib = "libgtkmacintegration.2.dylib"
+
+type
+  POSXApplication* = ptr TOSXApplication
+  TOSXApplication* = object of TGObject
+    private_data*: gpointer
+
+  POSXApplicationClass* = ptr TOSXApplicationClass
+  TOSXApplicationClass* = object of TGObjectClass
+  
+  TOSXApplicationAttentionType* = enum
+    CRITICAL_REQUEST = 0, INFO_REQUEST = 10
+
+proc get_type*(): GType{.cdecl, dynlib: lib,
+    importc: "gtkosx_application_get_type".}
+
+proc get*(): POSXApplication{.cdecl, dynlib: lib,
+    importc: "gtkosx_application_get".}
+
+proc ready*(application: POSXApplication){.cdecl, dynlib: lib,
+    importc: "gtkosx_application_ready".}
+
+proc set_use_quartz_accelerators*(application: POSXApplication,
+                                 value: gboolean){.cdecl, dynlib: lib,
+    importc: "gtkosx_set_use_quartz_accelerators".}
+
+proc use_quartz_accelerators*(application: POSXApplication): gboolean {.cdecl,
+    dynlib: lib, importc: "gtkosx_application_use_quartz_accelerators".}
+
+proc set_menu_bar*(application: POSXApplication, menu_shell: PMenuShell){.cdecl,
+    dynlib: lib, importc: "gtkosx_application_set_menu_bar".}
+
+proc sync_menubar*(application: POSXApplication){.cdecl, dynlib: lib,
+    importc: "gtkosx_application_sync_menubar".}
+
+proc insert_app_menu_item*(application: POSXApplication, menu_item: PWidget,
+                           index: gint){.cdecl, dynlib: lib,
+     importc: "gtkosx_application_insert_app_menu_item".}
+
+proc set_window_menu*(application: POSXApplication, menu_item: PMenuItem){.cdecl,
+    dynlib: lib, importc: "gtkosx_application_set_window_menu".}
+
+proc set_help_menu*(application: POSXApplication, menu_item: PMenuItem){.cdecl,
+    dynlib: lib, importc: "gtkosx_application_set_help_menu".}
+
+proc set_dock_menu*(application: POSXApplication, menu_shell: PMenuShell){.cdecl,
+    dynlib: lib, importc: "gtkosx_application_set_dock_menu".}
+
+proc set_dock_icon_pixbuf*(application: POSXApplication, pixbuf: PPixbuf){.cdecl,
+    dynlib: lib, importc: "gtkosx_application_set_dock_icon_pixbuf".}
+
+proc set_dock_icon_resource*(application: POSXApplication,
+                            name, resoucer_type, subdir : cstring){.cdecl, dynlib: lib,
+     importc: "gtkosx_application_set_dock_item_resource".}
+
+proc attention_request*(application: POSXApplication,
+                        kind: TOSXApplicationAttentionType): gint {.cdecl,
+    dynlib: lib, importc: "gtkosx_application_attention_request".}
+
+proc cancel_attention_request*(application: POSXApplication, id: gint){.cdecl,
+    dynlib: lib, importc: "gtkosx_application_cancel_attention_request".}
+
+proc get_bundle_path*(): cstring{.cdecl, dynlib: lib,
+    importc: "gtkosx_application_get_bundle_path".}
+
+proc get_resource_path*(): cstring{.cdecl, dynlib: lib,
+    importc: "gtkosx_application_get_resource_path(".}
+
+proc get_executable_path*(): cstring{.cdecl, dynlib: lib,
+    importc: "gtkosx_application_get_executable_path".}
+
+proc get_bundle_id*(): cstring{.cdecl, dynlib: lib,
+    importc: "gtkosx_application_get_bundle_id".}
+
+proc get_bundle_info*(key : cstring): cstring{.cdecl, dynlib: lib,
+    importc: "gtkosx_application_get_bundle_info".}
